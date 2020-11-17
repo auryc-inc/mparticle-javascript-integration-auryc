ac2738b79188d7bbe27b4bb47240f386a9aa4f69
Feng Shao <feng.shao@auryc.com>
2020-11-16 20:40:16 -0800

    implementing initialization

--- src/initialization.js
+++ src/initialization.js
@@ -1,5 +1,5 @@
 var initialization = {
-    name: 'insertSDKNameHere',
+    name: 'Auryc',
 /*  ****** Fill out initForwarder to load your SDK ******
     Note that not all arguments may apply to your SDK initialization.
     These are passed from mParticle, but leave them even if they are not being used.
@@ -15,23 +15,38 @@ var initialization = {
             /* Load your Web SDK here using a variant of your snippet from your readme that your customers would generally put into their <head> tags
                Generally, our integrations create script tags and append them to the <head>. Please follow the following format as a guide:
             */
+            var safeInvoke = function (g, fn) {
+                return function wrapper() {
+                    if (g.auryc && typeof g.auryc[fn] !== 'undefined' && g.auryc[fn] !== wrapper) {
+                        g.auryc[fn].apply(this, Array.prototype.slice.call(arguments, 0));
+                    }
+                    else {
+                        var args = arguments;
+                        g.aurycReadyCb.push(function () {
+                            g.auryc[fn].apply(this, Array.prototype.slice.call(args, 0));
+                        });
+                    }
+                };
+            };
 
-            // var clientScript = document.createElement('script');
-            // clientScript.type = 'text/javascript';
-            // clientScript.async = true;
-            // clientScript.src = 'https://www.clientscript.com/static/clientSDK.js';   // <---- Update this to be your script
-            // (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(clientScript);
-            // clientScript.onload = function() {
-            //     if (clientSDKObject && eventQueue.length > 0) {
-            //         // Process any events that may have been queued up while forwarder was being initialized.
-            //         for (var i = 0; i < eventQueue.length; i++) {
-            //             processEvent(eventQueue[i]);
-            //         }
-            //          // now that each queued event is processed, we empty the eventQueue
-            //         eventQueue = [];
-            //     }
-            //    clientSDKObject.initialize(forwarderSettings.apiKey);
-            // };
+            (function (g) {
+                g.aurycReadyCb = g.aurycReadyCb || [];
+                g.auryc = g.auryc || {};
+                var fns = ['track', 'identify', 'addFBCustomData', 'addUserProperties', 'addSessionProperties', 
+                    'addInternalSessionProperties', 'getReplayUrl', 'setFeedbackEnabled', 'clearUserCookie',
+                    'addFBSubmitHandler', 'addFBCancelHandler', 'pause', 'resume'];
+                fns.forEach(function (fn) {
+                    g.auryc[fn] = safeInvoke(g, fn);
+                });
+            })(window);
+
+            var clientScript = document.createElement('script');
+            clientScript.type = 'text/javascript';
+            clientScript.setAttribute('data-cfasync', 'false');
+            clientScript.setAttribute('charset', 'utf-8');
+            clientScript.async = true;
+            clientScript.src = 'https://cdn.auryc.com/' + forwarderSettings.siteId + '/container.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(clientScript);
         } else {
             // For testing, you should fill out this section in order to ensure any required initialization calls are made,
             // clientSDKObject.initialize(forwarderSettings.apiKey)
